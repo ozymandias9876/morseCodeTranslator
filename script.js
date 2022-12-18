481e6035522ba9b955c68060060452d358c9fc1a
Himanshu Tiwari <38527848+ozymandias9876@users.noreply.github.com>
2022-12-18 15:19:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,161 @@
+const textToMorse = (text) => {
+  // Create a map of the Morse code alphabet
+  const morseCode = {
+    'a': '.-',
+    'b': '-...',
+    'c': '-.-.',
+    'd': '-..',
+    'e': '.',
+    'f': '..-.',
+    'g': '--.',
+    'h': '....',
+    'i': '..',
+    'j': '.---',
+    'k': '-.-',
+    'l': '.-..',
+    'm': '--',
+    'n': '-.',
+    'o': '---',
+    'p': '.--.',
+    'q': '--.-',
+    'r': '.-.',
+    's': '...',
+    't': '-',
+    'u': '..-',
+    'v': '...-',
+    'w': '.--',
+    'x': '-..-',
+    'y': '-.--',
+    'z': '--..',
+    '0': '-----',
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...',
+    '8': '---..',
+    '9': '----.',
+    '.': '.-.-.-',
+    ',': '--..--',
+    '?': '..--..',
+    "'": '.----.',
+    '!': '-.-.--',
+    '/': '-..-.',
+    '(': '-.--.',
+    ')': '-.--.-',
+    '&': '.-...',
+    ':': '---...',
+    ';': '-.-.-.',
+    '=': '-...-',
+    '+': '.-.-.',
+    '-': '-....-',
+    '_': '..--.-',
+    '"': '.-..-.',
+    '$': '...-..-',
+    '@': '.--.-.',
+    ' ': '/'
+  };
+  
+  // Split the text into an array of characters
+  const characters = text.split('');
+  
+  // Map the characters to their corresponding Morse code values
+  const morse = characters.map((char) => {
+    return morseCode[char.toLowerCase()];
+  });
+  
+  // Join the array of Morse code values into a single string
+  return morse.join(' ');
+};
+const morseToText = (morse) => {
+  // Create a map of the Morse code alphabet
+  const morseCode = {
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '-----': '0',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '.-.-.-': '.',
+    '--..--': ',',
+    '..--..': '?',
+    '.----.': "'",
+    '-.-.--': '!',
+    '-..-.': '/',
+    '-.--.': '(',
+    '-.--.-': ')',
+    '.-...': '&',
+    '---...': ':',
+    '-.-.-.': ';',
+    '-...-': '=',
+    '.-.-.': '+',
+    '-....-': '-',
+    '..--.-': '_',
+    '.-..-.': '"',
+    '...-..-': '$',
+    '.--.-.': '@',
+    '/': ' '
+  };
+  
+  
+  const characters = morse.split(' ');
+  
+
+  const text = characters.map((char) => {
+    return morseCode[char];
+  });
+  
+  
+  return text.join('');
+};
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+form.addEventListener('submit', (event) => {
+  
+  event.preventDefault();
+  
+  
+  const value = input.value;
+  const type = document.querySelector('input[name="type"]:checked').value;
+  
+  let output;
+  if (type === 'text') {
+    output = textToMorse(value);
+  } else {
+    output = morseToText(value);
+  }
+  
+  document.getElementById('output').innerHTML = output;
+});
